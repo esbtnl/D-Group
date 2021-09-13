1f7b921c7130da99256b8542a38740a95fd083f8
Oscar Lönnqvist <osclo921@student.liu.se>
2021-09-13 13:35:20 +0200

    Upload to azure

--- src/components/Navbar/NavbarElements.js
+++ src/components/Navbar/NavbarElements.js
@@ -1,57 +1,53 @@
 import styled from "styled-components";
-import { NavLink as Link } from "react-router-dom"
-import {FaBars} from "react-icons/fa"
+import { NavLink as Link } from "react-router-dom";
+import { FaBars } from "react-icons/fa";
 
 export const Nav = styled.nav`
-    background: #964B00;
-    height: 125px;
-    width: 100%;
-    display: flex;
-    justify-content: center;
-    padding: 0.5rem calc((100vw - 1000px)/2);
-    z-index: 10;
-    @media screen and (min-width: 768px){
-        position: fixed;
-        background-color: transparent;        
-    }
-`
+  background: #964b00;
+  height: 125px;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  padding: 0.5rem calc((100vw - 1000px) / 2);
+  z-index: 10;
+`;
 
 export const NavLink = styled(Link)`
-    color: #fff;
-    display: flex;
-    align-items: center;
-    text-decoration: none;
-    padding: 0 0.5rem;
-    height: 100%;
-    cursor: pointer;
-    font-size: 1.1rem;
-    white-space: nowrap;
+  color: #fff;
+  display: flex;
+  align-items: center;
+  text-decoration: none;
+  padding: 0 0.5rem;
+  height: 100%;
+  cursor: pointer;
+  font-size: 1.1rem;
+  white-space: nowrap;
 
-    &.active {
-        color: #00F;
-    }
-`
+  &.active {
+    color: #e5398d;
+  }
+`;
 
 export const Bars = styled(FaBars)`
-    display: none;
-    color: #fff;
+  display: none;
+  color: #fff;
 
-    @media screen and (max-width: 768px){
-        display: block;
-        position: absolute;
-        top: 20px;
-        right: 0;
-        transform: translate(-100%, 75%);
-        font-size: 1.8rem;
-        cursor: pointer;
-    }
-`
+  @media screen and (max-width: 768px) {
+    display: block;
+    position: absolute;
+    top: 20px;
+    right: 0;
+    transform: translate(-100%, 75%);
+    font-size: 1.8rem;
+    cursor: pointer;
+  }
+`;
 
 export const NavMenu = styled.div`
-    display: flex;
-    align-items: center;
+  display: flex;
+  align-items: center;
 
-    @media screen and (max-width: 768px){
-        display: none;
-    }
-`
+  @media screen and (max-width: 768px) {
+    display: none;
+  }
+`;
